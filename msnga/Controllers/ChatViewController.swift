6a8548fdd4321b727e85acaf824d2e2a2a40ba1a
Agrim Asthana <agrimasthana@gmail.com>
2017-11-08 23:44:09 -0600

    UI improvements and color gradient

--- msnga/Controllers/ChatViewController.swift
+++ msnga/Controllers/ChatViewController.swift
@@ -22,7 +22,7 @@ final class ChatViewController: JSQMessagesViewController
  lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
  lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
 
-    override func collectionView(_ collectionView: JSQMessagesCollectionView, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData? {
         return messages[indexPath.item]
     }
     
@@ -89,12 +89,34 @@ final class ChatViewController: JSQMessagesViewController
         }
         if (message.accessibilityValue?.count != 0) {
             let sent = Float(message.accessibilityValue!)
-            if(sent! >= 0.1)
-            {
-                cell.textView?.backgroundColor = UIColor.green
-            }
-            else {
-                cell.textView?.backgroundColor = UIColor.red
+            cell.messageBubbleImageView.image? = (cell.messageBubbleImageView.image?.withRenderingMode(.alwaysTemplate))!
+            switch(sent!){
+            case -1 ... -0.75:
+                cell.messageBubbleImageView.tintColor = UIColor(red:(254/255.0), green: (16/255.0), blue: (0/255.0), alpha: 1.0)
+                cell.textView?.textColor = UIColor.white
+            case -0.75 ... -0.50:
+                cell.messageBubbleImageView?.tintColor = UIColor(red:(251/255.0), green: (79/255.0), blue: (7/255.0), alpha: 1.0)
+                cell.textView?.textColor = UIColor.white
+            case -0.50 ... -0.25:
+                cell.messageBubbleImageView?.tintColor = UIColor(red:(249/255.0), green: (138/255.0), blue: (13/255.0), alpha: 1.0)
+                cell.textView?.textColor = UIColor.white
+            case -0.25...0.09:
+                cell.messageBubbleImageView?.tintColor = UIColor(red:(247/255.0), green: (194/255.0), blue: (19/255.0), alpha: 1.0)
+                cell.textView?.textColor = UIColor.black
+            case 0.00...0.25:
+                cell.messageBubbleImageView?.tintColor = UIColor(red: (244/255.0), green: (245/255.0), blue: (25/255.0), alpha: 1.0)
+                cell.textView?.textColor = UIColor.black
+            case 0.25...0.50:
+                cell.messageBubbleImageView?.tintColor = UIColor(red:(192/255.0), green: (243/255.0), blue: (30/255.0), alpha: 1.0)
+                cell.textView?.textColor = UIColor.black
+            case 0.50...0.75:
+                cell.messageBubbleImageView?.tintColor = UIColor(red:(143/255.0), green: (241/255.0), blue: (36/255.0), alpha: 1.0)
+                cell.textView?.textColor = UIColor.black
+            case 0.75...1:
+                cell.messageBubbleImageView?.tintColor = UIColor(red:(99/255.0), green: (239/255.0), blue: (42/255.0), alpha: 1.0)
+                cell.textView?.textColor = UIColor.black
+            default:
+                cell.messageBubbleImageView?.tintColor = UIColor.blue
             }
         }
         
@@ -138,7 +160,7 @@ final class ChatViewController: JSQMessagesViewController
         messageRefHandle = msgqry.observe(.childAdded, with: {(snapshot) -> Void in
             let msgdata = snapshot.value as! Dictionary<String,String>
             if let id = msgdata["senderId"] as String!, let name = msgdata["senderName"] as String!, let text = msgdata["text"] as String!, let sent = msgdata["sentimentScore"] as String!, text.count > 0 {
-                self.addMessage(withId: id, name: name, text: text, sent: sent ?? "0")
+                self.addMessage(withId: id, name: name, text: text, sent: sent )
                 self.finishReceivingMessage()
             }
             else{
